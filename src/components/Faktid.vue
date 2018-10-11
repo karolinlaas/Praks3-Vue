77f95d23e6671d2e0ae1aa3e28ec238b1aff7d09
Karolin Laas <karolin.laas@khk.ee>
2018-10-11 13:57:16 +0300

    Loodud numbri faktide aplikatsioon.

---
+++ src/components/Faktid.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Faktid numbrite kohta</h1>
+    <p>Huvitavad faktid iga numbri kohta</p>
+    <input type="number" v-model="number">
+    <p v-cloak>{{ data }}</p>
+  </div>
+</template>
+
+<script>
+  const axios = require('axios')
+  
+export default {
+  name: 'Faktid',
+  data () {
+    return {
+      data: '',
+      number: 0
+    }
+  },
+  watch: {
+    number() {
+      if (this.number !== '') {
+        let url = `http://numbersapi.com/${this.number}`
+
+        axios.get(url)
+          .then(response => {
+            this.data = response.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  div {
+    text-align: center;
+    margin-top: 25vh;
+  }
+  [v-cloak] {
+    display: none;
+  }
+</style>
